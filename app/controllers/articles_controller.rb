65ebb60eb407713c195b1f79dde75040e834e61d
dienigma <joshichinmay09@gmail.com>
2018-11-09 11:28:01 +0000

    Added a show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,21 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to article_show(@article)
+    # redirect_to article_show(@article)
+    if @article.save
+      flash[:notice] = "Article Created"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
+    
   end
   
+  def show
+      
+    @article = Article.find(params[:id])
+    
+  end
   private
   
   def article_params
